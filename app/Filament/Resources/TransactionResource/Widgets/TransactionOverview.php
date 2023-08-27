c0960bb5b5711c9d5f83ae6b2a3db5b7170dfaac
Shaun <shauncuier@gmail.com>
2023-08-28 04:20:16 +0600

    Widgets 1

---
+++ app/Filament/Resources/TransactionResource/Widgets/TransactionOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\TransactionResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class TransactionOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
