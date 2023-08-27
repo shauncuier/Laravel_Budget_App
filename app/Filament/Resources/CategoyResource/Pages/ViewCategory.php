e5c7535bc2eb726135268844bea412f22d2ac6a3
Shaun <shauncuier@gmail.com>
2023-08-28 03:22:38 +0600

    Filament Practice

---
+++ app/Filament/Resources/CategoyResource/Pages/ViewCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CategoyResource\Pages;
+
+use App\Filament\Resources\CategoyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewCategory extends ViewRecord
+{
+    protected static string $resource = CategoyResource::class;
+}
