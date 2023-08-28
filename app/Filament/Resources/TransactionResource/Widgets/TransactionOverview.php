b84c294398dfebf789daeee16fa53194539b9610
Shaun <shauncuier@gmail.com>
2023-08-29 00:44:15 +0600

    Filament Practice

--- app/Filament/Resources/TransactionResource/Widgets/TransactionOverview.php
+++ app/Filament/Resources/TransactionResource/Widgets/TransactionOverview.php
@@ -13,14 +13,14 @@ class TransactionOverview extends BaseWidget
 
     protected function getTablePage(): string
     {
-       return TransactionResource\Pages\ListTransactions::class;
+        return TransactionResource\Pages\ListTransactions::class;
     }
 
     protected function getStats(): array
     {
         return [
             Stat::make('Total Transactions', $this->getPageTableQuery()->count()),
-            Stat::make('Total Amount', '$ '.number_format($this->getPageTableQuery()->sum('amount'),2) )
+            Stat::make('Total Amount', '$ ' . number_format($this->getPageTableQuery()->sum('amount'), 2))
         ];
     }
     protected function getColumns(): int
